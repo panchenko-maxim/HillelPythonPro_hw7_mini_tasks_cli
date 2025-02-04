3c57311e8f9846e16e1ba656e3bb0af0d98b11ae
root221 <panchenko.maxim87@gmail.com>
2025-02-04 11:11:45 +0200

    end 3 task (after delete function from main)

--- app/cli/who_is_here/main.py
+++ app/cli/who_is_here/main.py
@@ -3,52 +3,6 @@ import typer
 from app.services.check_astros_file import check_astros_file
 from app.services.text_processing_for_astro_data import text_processing_for_astro_data
 
-# def get_day_seconds_now() -> int:
-#     time_now = datetime.now()
-#     return time_now.hour * 3600 + time_now.minute * 60 + time_now.second
-
-# def check_different_seconds(check_seconds: int, time_for_check: int, time_now: int) -> tuple[int, bool]:
-#     if time_now - time_for_check > check_seconds:
-#         return time_now, True
-#     return time_for_check, False
-
-# def save_and_return_astro_file() -> None:
-#     with requests.Session() as session, session.get(url="http://api.open-notify.org/astros.json") as response:
-#         astros_data = response.json()
-#         astros_data["save_time"] = get_day_seconds_now()
-#         with Path(ROOT_DIR / "files_output/astros.json").open("w") as file:
-#             json.dump(astros_data, file, indent=4)
-#         return astros_data
-
-
-# def check_astros_file() -> None:
-#     try:
-#         with Path(ROOT_DIR / "files_output/astros.json").open("r") as file:
-#             data_from_json = json.load(file)
-#             seconds_today = get_day_seconds_now()
-#             time, change = check_different_seconds(check_seconds=10,
-#                                                    time_for_check=data_from_json["save_time"],
-#                                                    time_now=seconds_today)
-#             if change:
-#                 return save_and_return_astro_file()
-#             return data_from_json
-#     except FileNotFoundError:
-#         return save_and_return_astro_file()
-
-# def new_astros_data_format(new_astros_data: dict, craft: str, name: str) -> None:
-#     if craft not in new_astros_data:
-#         new_astros_data[craft] = set()
-#     new_astros_data[craft].add(name)
-
-# def text_processing_for_astro_data(astros_data: json) -> str:
-#     new_astros_data = {}
-#     for astros in astros_data["people"]:
-#         new_astros_data_format(new_astros_data=new_astros_data, craft=astros["craft"], name=astros["name"])
-#     text = ""
-#     for craft, names in new_astros_data.items():
-#         text += f"Craft '{craft}': {'; '.join(names)[:-1]}\n"
-#     return text.rstrip()
-
 def who_is_here() -> None:
     astros_data = check_astros_file()
     typer.echo(f"Hello, now in space {astros_data['number']} astronauts\n"
