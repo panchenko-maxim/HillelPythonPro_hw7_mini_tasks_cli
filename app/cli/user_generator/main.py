e65652a81b99ee6c5277f0a061ef8033b67f4f4d
panchenko-maxim <panchenko.maxim87@google.com>
2025-02-04 21:57:43 +0200

    task "user_generator" before refactor function

--- app/cli/user_generator/main.py
+++ app/cli/user_generator/main.py
@@ -5,14 +5,21 @@ from app.services.save_file import save_file
 
 faker = Faker()
 
-def user_generator(amount_of_users: int, format_file: str|None) -> None:
-    users = [[faker.first_name(), faker.email()] for el in range(amount_of_users)]
-    users_text = "".join([f"{user[0]} {user[1]}\n" for user in users]).rstrip("\n")
+def get_users_and_emails_from_faker(amount_of_users: int) -> tuple[list[list[str]], str]:
+    users_list = [[faker.first_name(), faker.email()] for el in range(amount_of_users)]
+    users_text = "".join([f"{user[0]} {user[1]}\n" for user in users_list]).rstrip("\n")
+    return users_list, users_text
+
+def save_and_return_name_and_emails_of_users(amount_of_users: int, format_file: str|None) -> str:
+    users_list, users_text = get_users_and_emails_from_faker(amount_of_users=amount_of_users)
     if format_file:
         save_file(name_file="user_generator",
                   format_file=format_file,
-                  data_for_save=users_text if "txt" in format_file else users)
-    else:
-        typer.echo(users_text)
+                  data_for_save=users_text if "txt" in format_file else users_list)
+        return f"Saved {amount_of_users} users in 'files_output/{format_file}'"
+    return users_text
+
+def user_generator(amount_of_users: int, format_file: str|None) -> None:
+    typer.echo(save_and_return_name_and_emails_of_users(amount_of_users=amount_of_users, format_file=format_file))
 
 
